586b87de6262f026cb67ef9390f220d377fc556d
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-02-10 16:36:49 -0600

    clean up theme

--- lib/theme.ts
+++ lib/theme.ts
@@ -4,26 +4,6 @@ const theme = extendTheme({
   fontFamily: {
     body: "'Public Sans', var(--joy-fontFamily-fallback)",
   },
-  colorSchemes: {
-    light: {
-      palette: {
-        primary: {
-          50: "#C0CCD9",
-          100: "#A5B8CF",
-          200: "#6A96CA",
-          300: "#4886D0",
-          400: "#2178DD",
-          500: "#096BDE",
-          600: "#1B62B5",
-          700: "#265995",
-          800: "#2F4968",
-          900: "#2F3C4C",
-        },
-      },
-    },
-  },
 });
-
-// Then, pass it to `<CssVarsProvider theme={theme}>`.
 
 export default theme;
