7a8a8ded582cdf8c36be53f244b7819284fc6fef
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-02-10 14:47:23 -0600

    kill styles and src

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,10 @@ import * as React from "react";
 import Head from "next/head";
 import { AppProps } from "next/app";
 import { CssVarsProvider } from "@mui/joy/styles";
-import theme from "../src/theme";
+import theme from "../lib/theme";
 import CssBaseline from "@mui/joy/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
-import createEmotionCache from "../src/createEmotionCache";
-import PlausibleProvider from "next-plausible";
+import createEmotionCache from "../lib/createEmotionCache";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -22,12 +21,10 @@ export default function MyApp(props: MyAppProps) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <PlausibleProvider domain="sycamore.garden">
         <CssVarsProvider theme={theme}>
           <CssBaseline />
           <Component {...pageProps} />
         </CssVarsProvider>
-      </PlausibleProvider>
     </CacheProvider>
   );
 }
