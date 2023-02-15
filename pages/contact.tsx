cc7f21e33a29908bd6bb9e8bb25ada8549fd8105
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-02-15 15:26:05 -0600

    contact list

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,36 +4,51 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
 import List from "@mui/joy/List";
-import ListItem from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
+import ListItemButton from "@mui/joy/ListItemButton";
+import ListItemDecorator from "@mui/joy/ListItemDecorator";
+import ListItemContent from "@mui/joy/ListItemContent";
 import Typography from "@mui/joy/Typography";
 
 export default function Contact() {
   return (
     <Layout>
       <Typography level="h2" sx={{ mb: "1rem" }}>
-        Posts
+        Contact
       </Typography>
       Here's how you can get in touch with me:
       <List>
         <ListItem>
-          <a href="">
-            <EmailIcon /> name@email.com
-          </a>
+          <ListItemDecorator>
+            <EmailIcon />
+          </ListItemDecorator>
+          <ListItemContent>
+            <a href="mailto:name@email.com">name@email.com</a>
+          </ListItemContent>
         </ListItem>
         <ListItem>
-          <a href="">
-            <GitHubIcon /> GitHub_username
-          </a>
+          <ListItemDecorator>
+            <GitHubIcon />
+          </ListItemDecorator>
+          <ListItemContent>
+            <a href="https://github.com/">GitHub username</a>
+          </ListItemContent>
         </ListItem>
         <ListItem>
-          <a href="">
-            <LinkedInIcon /> LinkedIn_username
-          </a>
+          <ListItemDecorator>
+            <LinkedInIcon />
+          </ListItemDecorator>
+          <ListItemContent>
+            <a href="https://linkedin.com/">LinkedIn username</a>
+          </ListItemContent>
         </ListItem>
         <ListItem>
-          <a href="">
-            <TwitterIcon /> Twitter_username
-          </a>
+          <ListItemDecorator>
+            <TwitterIcon />
+          </ListItemDecorator>
+          <ListItemContent>
+            <a href="https://twitter.com/">Twitter username</a>
+          </ListItemContent>
         </ListItem>
       </List>
     </Layout>
