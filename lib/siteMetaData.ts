f5222e25223ff772811dcdf926d0468250cf601d
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-02-16 14:03:12 -0600

    refactoring og paths

--- lib/siteMetaData.ts
+++ lib/siteMetaData.ts
@@ -4,8 +4,8 @@ export const siteMetaData = {
   description: "Minimalist Joy UI blog built with Next.js in TypeScript.",
   language: "en-us",
   siteUrl: "https://joy-next-blog.vercel.app",
-  image: "/images/photo.jpeg",
-  ogImage: "https://joy-next-blog.vercel.app/images/og-card.jpg",
-  twImage: "https://joy-next-blog.vercel.app/images/og-card.jpg",
+  image: "images/photo.jpeg",
+  ogImage: "images/og-card.jpg",
+  twImage: "images/og-card.jpg",
   locale: "en-US",
 };
