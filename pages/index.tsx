18dc151612039209c1693b27d2dd70564c19c6c1
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-03-03 15:16:34 -0600

    vercel you're kinda being a hater right now

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Index() {
         Features
       </Typography>
       <List>
-        <ListItem>✓ Built in TypeScript</ListItem>
+        <ListItem>✓ Built with TypeScript</ListItem>
         <ListItem>✓ Designed with Joy UI's default styles</ListItem>
         <ListItem>✓ Ready to publish with Next.js Markdown blog</ListItem>
         <ListItem>✓ Light and dark modes with toggle button</ListItem>
